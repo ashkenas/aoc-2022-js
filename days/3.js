35e45760dc53d254996e450f1dca38627f6f6e11
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-12-03 00:14:16 -0500

    Day 3

---
+++ days/3.js
@@ -0,0 +1,26 @@
+const input = document.body.innerText.trim().split('\n');
+
+let p1 = 0;
+let p2 = 0;
+for (let i = 0; i < input.length; i++) {
+    const f = input[i].substr(0, input[i].length / 2);
+    const s = input[i].substr(input[i].length / 2);
+    for (const c of f) {
+        if (s.includes(c)) {
+            p1 += c.charCodeAt(0) - (/[a-z]/.test(c) ? 96 : 38);
+            break;
+        }
+    }
+    if (!(i % 3)) {
+        for (const c of input[i]) {
+            if (input[i + 1].includes(c) && input[i + 2].includes(c)) {
+                p2 += c.charCodeAt(0) - (/[a-z]/.test(c) ? 96 : 38);
+                break;
+            }
+        }
+    }
+}
+
+console.log(`Advent of Code Day 3:
+Part 1: ${p1}
+Part 2: ${p2}`);
